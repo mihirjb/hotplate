ed940eebd831eda721244c2773c3a728e197bdb7
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-12 02:14:35 +0530

    removed another migration

--- db/migrate/20130809061421_remove_rest_id_from_menuitem.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRestIdFromMenuitem < ActiveRecord::Migration
-  def up
-    remove_column :menuitems, :rest_id
-  end
-
-  def down
-    add_column :menuitems, :rest_id, :integer
-  end
-end
