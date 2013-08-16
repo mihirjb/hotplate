7bdd33a233fda7686139b629b3686dfd7b6f9493
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-16 19:11:17 +0530

    model and validation changes 3

--- db/migrate/20130816131857_remove_column.rb
+++ db/migrate/20130816131857_remove_column.rb
@@ -1,7 +1,5 @@
 class RemoveColumn < ActiveRecord::Migration
   def self.up
-    remove_column :restaurants, :restaddressline1
-    remove_column :restaurants, :restaddressline2
     remove_column :restaurants, :resttimingfrom
     remove_column :restaurants, :resttiminigto
 
