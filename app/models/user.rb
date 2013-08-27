c5fed52e70427aefe7143c968d15581a1f4a9011
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-28 04:35:13 +0530

    changed name to userfullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,8 @@ class User < ActiveRecord::Base
    
    valid_email_format = /[\w+\-.]+@[a-z\-.]+\.[a-z]+/i
 
-   validates_presence_of :name
-   validates_length_of :name, :maximum => 50
+   validates_presence_of :userfullname
+   validates_length_of :userfullname, :maximum => 50
    validates :email, :presence => true,:format => valid_email_format, :uniqueness => true
    validates_length_of :password,:in => 6..40
    validates :password, :confirmation  => true
