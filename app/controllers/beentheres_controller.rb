88eb9f818d07d41c3a6e1f007cb73538b2a69108
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-31 15:32:05 +0530

    trying again with been

--- app/controllers/beentheres_controller.rb
+++ app/controllers/beentheres_controller.rb
@@ -1,13 +1,14 @@
 class BeentheresController < ApplicationController
   before_filter :authenticate_user!
   def create
-      @instance = current_user.beentheres.find_by_restaurant_id(params[:restaurant_id])
-      if @instance  
-        @instance.destroy
-      else
-      current_user.beentheres.create(:restaurant_id => params[:restaurant_id])
-        render :layout => false
-    end
+    @instance = current_user
+     @alreadypresent = Beenthere.find_by_restaurant_id("#{params[:restaurant_id]}",:conditions => ['restaurant_id iLIKE ? AND user_id iLIKE ?', "#{params[:restaurant_id]}", "#{@instance.id}"])
+     if @alreadypresent
+        @alreadypresent.destroy
+    else
+    Beenthere.create(:restaurant_id => params[:restaurant_id], :user_id => @instance.id)
+    render :layout => false
+  end
   end
 
   def destroy
