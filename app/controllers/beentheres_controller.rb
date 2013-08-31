dd0ed181524bbd7fb4088506cf80fe3f4250dc01
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-31 15:04:29 +0530

    fixed show and beencontroller for iLIKe

--- app/controllers/beentheres_controller.rb
+++ app/controllers/beentheres_controller.rb
@@ -2,7 +2,7 @@ class BeentheresController < ApplicationController
   before_filter :authenticate_user!
   def create
     @instance = current_user
-     @alreadypresent = Beenthere.find_by_restaurant_id("#{params[:restaurant_id]}",:conditions => ['restaurant_id LIKE ? AND user_id LIKE ?', "#{params[:restaurant_id]}", "#{@instance.id}"])
+     @alreadypresent = Beenthere.find_by_restaurant_id("#{params[:restaurant_id]}",:conditions => ['restaurant_id iLIKE ? AND user_id iLIKE ?', "#{params[:restaurant_id]}", "#{@instance.id}"])
      if @alreadypresent
         @alreadypresent.destroy
     else
