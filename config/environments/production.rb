f8bc1c0f379a5b2d0647af864d9ddd8e2e4f6e4b
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-08-31 22:14:43 +0530

    trying with gmail again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ Hotplate::Application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address: "box497.bluehost.com",
+      address: "smtp.gmail.com",
       port: 587,
       domain: "5ve.in",
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: ENV["sales@5ve.in"],
-      password: ENV["Connect*5"]
+      user_name: ENV["SMTP_USERNAME"],
+      password: ENV["SMTP_PASSWORD"]
     }
   
   
